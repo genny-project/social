3134510f2feaab4c3d8b9811e397ecd214deef5a
adamcrow64 <adamcrow63@gmail.com>
2017-08-16 22:54:16 +1000

    Updated dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+echo "RUNNING DOCKER-ENTRYPOINT"
+#set -e;
 
-KEYCLOAK_JSON_FILE=/realm/keycloak.json
-CLUSTER_XML_FILE=/cluster.xml
-JAVA_OPT_KEYCLOAK_PATH=-Dswarm.keycloak.json.path=${KEYCLOAK_JSON_FILE}
+KEYCLOAK_JSON_DIR=/realm
+KEYCLOAK_ORIGINAL_JSON_DIR=/opt/realm
+
+# copy all the keycloak files so they may be modified
+cp -rf ${KEYCLOAK_ORIGINAL_JSON_DIR}/* ${KEYCLOAK_JSON_DIR}/
 
 
 # change the package.json file
@@ -28,28 +32,12 @@ function change_line2 {
     /bin/sed -i  '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
 }
 
-if [ -z "${KEYCLOAKURL}" ]; then
-   echo "No KEYCLOAKURL given. No change to keycloak.json"
-else
+for i in `ls ${KEYCLOAK_JSON_DIR}` ; do
    OLD_LINE_KEY="auth-server-url"
-   NEW_LINE="\"auth-server-url\": \"${KEYCLOAKURL}\","
-   change_line "\${OLD_LINE_KEY}" "\${NEW_LINE}" "\${KEYCLOAK_JSON_FILE}"
-fi
+   NEW_LINE="\"auth-server-url\": \"${KEYCLOAKURL}/auth\","
+   change_line "\${OLD_LINE_KEY}" "\${NEW_LINE}" "\${i}"
+done
 
-   OLD_LINE_KEY="<interface>10\.1\.1\.\*<\/interface>"
-   NEW_LINE="<interface>${HOSTIP}</interface>"
-   change_line2 "\${OLD_LINE_KEY}" "\${NEW_LINE}" "\${CLUSTER_XML_FILE}"
-
-#Set some ENV by extracting from keycloak.json file
-export KEYCLOAK_REALM=`jq '.realm' /realm/keycloak.json`
-export KEYCLOAK_URL=`jq '.["auth-server-url"]' ${KEYCLOAK_JSON_FILE}`
-export KEYCLOAK_CLIENTID=`jq '.resource' ${KEYCLOAK_JSON_FILE}`
-export KEYCLOAK_SECRET=`jq '.credentials.secret' ${KEYCLOAK_JSON_FILE}`
-
-echo "KEYCLOAK REALM= ${KEYCLOAK_REALM}"
-echo "KEYCLOAK URL= ${KEYCLOAK_URL}"
-echo "KEYCLOAK CLIENTID= ${KEYCLOAK_CLIENTID}"
-echo "KEYCLOAK SECRET= ${KEYCLOAK_SECRET}"
 
 command="$1"; 
 if [ "$command" != "java" ]; then 
