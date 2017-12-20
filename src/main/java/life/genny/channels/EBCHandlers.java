fd342b126a3ce3e6df1d57d5ae83d0805a1bfe79
Adam Crow <acrow@Adams-Compooter.local>
2017-12-20 16:42:18 +1100

    Changed deserialiser

--- src/main/java/life/genny/channels/EBCHandlers.java
+++ src/main/java/life/genny/channels/EBCHandlers.java
@@ -71,10 +71,11 @@ public class EBCHandlers {
 	static String token;
 	static Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
 		@Override
-		public LocalDateTime deserialize(final JsonElement json, final Type type,
-				final JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-			return ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString()).toLocalDateTime();
-		}
+        public LocalDateTime deserialize(final JsonElement json, final Type type,
+	              final JsonDeserializationContext jsonDeserializationContext)
+	              throws JsonParseException {
+	            return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+	          }
 
 		public JsonElement serialize(final LocalDateTime date, final Type typeOfSrc, final JsonSerializationContext context) {
 			return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)); // "yyyy-mm-dd"
@@ -189,7 +190,7 @@ public class EBCHandlers {
 		String targetCode= msg.getString("targetCode");
 		Boolean expired= msg.getBoolean("expired");
 		Boolean refused= msg.getBoolean("refused");
-		String weight= msg.getString("weight");
+		Double weight= msg.getDouble("weight");
 		String token= msg.getString("token");
 		
 		System.out.println(targetCode);
