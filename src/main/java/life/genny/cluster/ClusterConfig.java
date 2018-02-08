43af3ba72e33e3da891e86f400fe3350282b494e
Anish Maharjan <anish.maharjan12341@gmail.com>
2018-02-08 17:04:21 +1100

    added username to cluster config

--- src/main/java/life/genny/cluster/ClusterConfig.java
+++ src/main/java/life/genny/cluster/ClusterConfig.java
@@ -18,7 +18,8 @@ public class ClusterConfig {
 
   private static String hostIP =
       System.getenv("HOSTIP") != null ? System.getenv("HOSTIP") : "127.0.0.1";
-
+  private static String systemUser =
+    	      System.getenv("USER") != null ? System.getenv("USER") : "genny";
   private static String privateIP = System.getenv("MYIP");
   private final static int portHazelcastCluster = 5703;
   private final static int portEBCluster = 15703;
@@ -28,6 +29,7 @@ public class ClusterConfig {
    */
   public static Config configHazelcastCluster() {
     final Config hazelcastConfig = new Config();
+    hazelcastConfig.getGroupConfig().setName( systemUser).setPassword(systemUser);
     hazelcastConfig.getNetworkConfig().setPort(portHazelcastCluster);
     hazelcastConfig.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
     hazelcastConfig.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
